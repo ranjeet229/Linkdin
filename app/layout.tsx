b83bd4b55eff4ab102f80bf2a84880ac2d6aa5d4
Ranjeet <kiranjeetkr80@gmail.com>
2025-08-09 23:10:12 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +25,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full`}
       >
-        <Navbar/>
-        {children}
-      </body>
-    </html>
+        <body className="min-h-screen flex flex-col">
+          <Navbar />
+          <div className="flex-1 w-full">
+            <main className="max-w-6xl mx-auto">{children}</main>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import SearchInput from "./SearchInput";
+import Navitems from "./Navitems";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="fixed w-full bg-white z-50">
+      <div className=" flex items-center max-w-6xl justify-between h-14 mx-auto px-3">
+        <div className="flex items-center gap-2">
+          <Image src={"/icon.jpg"} alt="Logo" width={40} height={40} />
+          <div>
+            <SearchInput />
+          </div>
+        </div>
+        <div className="flex items-center gap-5">
+          <div>
+            <Navitems />
+          </div>
+          <div>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <Button className="rounded-full cursor-pointer" variant={'secondary'} >
+                <SignInButton/>
+              </Button>
+            </SignedOut>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
